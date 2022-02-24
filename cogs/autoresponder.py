505a2cd27b0f3ba76c6ecdd62b2648674d84c2ad
Nilendu Ghoshal <ghoshalsubhranil@gmail.com>
2022-02-24 10:13:53 +0530

    Implemented conenction with MongoDB to remove dependency on JSON

--- cogs/autoresponder.py
+++ cogs/autoresponder.py
@@ -1,204 +1,133 @@
-import discord
-import json
-import asyncio
 import re
+import asyncio
 import datetime
+
+import discord
 from discord.ext import commands
+
 from utils.utils import (
 	guildid,
 	admincheck,
 	PaginatorView
 	)
 
+
 class Autoresponder(commands.Cog):
+
+	
 	def __init__(self, bot):
 		self.bot = bot
-		self.nonepair = {"trigger": None, "response": None}
 
-	@commands.Cog.listener()
-	async def on_message(self, ctx: discord.Message):
-		checklist = [i["_id"] for i in self.bot.db["Guild settings"].find({"autoresponder": True})]
-		checklist2 = [i["_id"] for i in self.bot.db["Guild settings"].find({"dadmode": True})]
 		
-		if ctx.channel.type == discord.ChannelType.private:
-			return
-		id = guildid(ctx.guild.id)
-		if id not in checklist:
-			return
-
-		flag1, flag2, message = False, False, ctx.content
-		with open("data/autoresponses.json", "r") as foo:
-			content = json.loads(foo.read())
-		
-		if ctx.guild == None or str(id) not in content.keys():
-			return
-		
-		if ctx.author.bot:
-			return
-
-		nonwildkeys, nonwildresp = [i["trigger"] for i in content[f"{id}"]["normal"]], [i["response"] for i in content[f"{id}"]["normal"]]
-
-		wildkeys, wildresp = [i["trigger"] for i in content[f"{id}"]["wildcard"]], [i["response"] for i in content[f"{id}"]["wildcard"]]
-
-		if any([message.lower() == "oop-", message.lower().startswith("oop- "), message.lower().endswith("oop-"), " oop- " in message.lower()]):
-			await ctx.add_reaction("<:eyez:684985729780154370>")
-			return
-
-		if message.lower() in nonwildkeys:
-			try:
-				reply = eval(nonwildresp[nonwildkeys.index(message.lower())])
-			except:
-				reply = nonwildresp[nonwildkeys.index(message.lower())]
-			async with ctx.channel.typing():
-				await asyncio.sleep(1)
-			await ctx.reply(reply)
-			return
-		
-		for i in wildkeys:
-			if any ([message.lower().startswith(f"{i} "), message.lower().endswith(f" {i}"), message.lower().endswith(f" {i}."), f" {i} " in message.lower(), i == message.lower()]):
-				flag1, index = True, wildkeys.index(i)
-				break
-		
-		if flag1 == True:
-			try:
-				reply = eval(wildresp[index])
-			except:
-				reply = wildresp[index]
-			async with ctx.channel.typing():
-				await asyncio.sleep(1)
-			await ctx.reply(reply)
-			flag1 = False
-			return
-		
-		if id in checklist2:
-			if len(message.lower()) <= 2035:
-				if any([message.lower().startswith("i\'m "), message.lower().startswith("im "), message.lower().startswith("i am ")]):
-					for i in ["im ", "i am ", "i\'m "]:
-						match = re.search(i, message.lower())
-						if match is not None:
-							pos = match.end()
-							flag2 = True
-							break
-			
-				elif any([" im " in message.lower(), " i am " in message.lower(), " i'm " in message.lower()]):
-					for i in [" im ", " i am ", " i\'m "]:
-							match = re.search(i, message.lower())
-							if match is not None:
-								pos = match.end()
-								flag2 = True
-								break
-		
-			if flag2 == True:
-				reply = f"Hi {message[pos:]}, I'm dad!"
-				await ctx.reply(reply)
-				flag2 = False
-				return
-
-	@commands.command(aliases = ("addresp",))
+	@commands.command(aliases = ('addresp',))
+	@commands.guild_only()
 	@admincheck()
-	async def addresponse(self, ctx: commands.Context, trigger, response, wildcard: str = None):
-		trigger = trigger.lower()
-		if any(["__" in response, "lambda" in response]):
-			await ctx.send("Cannot add that autoresponse")
-			return
-
+	async def addresponse(self, ctx, trigger, response, wildcard = None):
 		id = guildid(ctx.guild.id)
 		if wildcard == None or wildcard.lower() != "wildcard":
-			ctype, ctype2 = "normal", "wildcard"
+			wildcard = False
 		else:
-			ctype, ctype2 = "wildcard", "normal"
-		pair = {"trigger": trigger, "response": response}
-		with open("data/autoresponses.json", "r") as foo:
-			content = json.loads(foo.read())
-		try:
-			assert str(id) in content.keys()
-		except AssertionError:
-			content.update({f"{id}": {ctype: [pair], ctype2: [self.nonepair]}})
-		else:
-			content[str(id)][ctype].append(pair)
-			for i in content[str(id)][ctype]:
-				if i == self.nonepair:
-					content[str(id)][ctype].remove(i)
-		with open("data/autoresponses.json", "w") as foo:
-			foo.write(json.dumps(content, indent = 2))
-		await asyncio.sleep(0.5)
-		await ctx.send("Added response!")
+			wildcard = True
+		self.bot.db['autoresponder'].insert_one({
+			'guild': id,
+			'wildcard': wildcard,
+			'trigger': trigger,
+			'response': response
+		})
+		await ctx.send("Autoresponse successfully added")
 
-	@commands.command(aliases = ("removeresp",))
+			
+	@commands.command(aliases = ('removeresp',))
+	@commands.guild_only()
 	@admincheck()
-	async def removeresponse(self, ctx: commands.Context, *, trigger: str):
+	async def removeresponse(self, ctx, *, trigger):
 		id = guildid(ctx.guild.id)
-		with open("data/autoresponses.json", "r") as foo:
-			content = json.loads(foo.read())
+		out = self.bot.db['autoresponder'].find_one_and_delete({
+			'guild': id,
+			'trigger': trigger
+		})
 		try:
-			assert str(id) in content.keys()
+			assert out != None
 		except AssertionError:
-			await ctx.send("This guild does not have any autoresponse!")
-			return
+			await ctx.send("No such trigger found")
 		except:
 			raise
 		else:
-			ctype, ctype2 = "normal", "wildcard"
-			normlist, wildlist = [i["trigger"] for i in content[str(id)][ctype]], [i["trigger"] for i in content[str(id)][ctype2]]
-			try:
-				assert trigger in normlist+wildlist
-			except AssertionError:
-				await ctx.send("No such autoresponse found!")
-				return
-			except:
-				raise
-			else:
-				if trigger in normlist:
-					content[str(id)][ctype].pop(normlist.index(trigger))
-				else:
-					content[str(id)][ctype2].pop(wildlist.index(trigger))
-					
-				if content[str(id)][ctype] != [] and content[str(id)][ctype2] == []:
-					content[str(id)][ctype2].append(self.nonepair)
-				
-				elif content[str(id)][ctype] == [] and content[str(id)][ctype2] != []:
-					content[str(id)][ctype].append(self.nonepair)
-				
-				if content[str(id)][ctype] == content[str(id)][ctype2] == [self.nonepair]:
-					content.pop(str(id))
-				
-		with open("data/autoresponses.json", "w") as foo:
-					foo.write(json.dumps(content, indent = 2))
-		await asyncio.sleep(0.5)
-		await ctx.send("Removed response!")
-				
-	@commands.command(aliases = ("responses", "resps", "autoresps"))
+			await ctx.send("Trigger successfully removed")
+
+			
+	@commands.command(aliases = ('responses', 'resps'))
 	@commands.guild_only()
-	async def autoresponses(self, ctx: commands.Context):
+	async def responselist(self, ctx):
 		id = guildid(ctx.guild.id)
-		with open("data/autoresponses.json", "r") as foo:
-			content = json.loads(foo.read())
-		try:
-			assert str(id) in content.keys()
-		except AssertionError:
-			await ctx.send("No autoresponse for this guild! Try using `h!addresponse` to create a new response")
-		else:
-			nonwild = ["• `\"{}\"` — `\"{}\"`".format(i["trigger"], i["response"]) for i in content[str(id)]["normal"]]
-			wild = ["• `\"{}\"` — `\"{}\"`".format(i["trigger"], i["response"]) for i in content[str(id)]["wildcard"]]
-			normresp = "__**Normal autoresponses:**__\n"+"\n".join(nonwild)
-			wildresp = "__**Wildcards:**__\n"+"\n".join(wild)
-			response = normresp + "\n\n" + wildresp
-			response = response.replace("`\"None\"` — `\"None\"`", "None")
-			try:
-				assert len(response) <= 4096
-			except AssertionError:
-				view = PaginatorView(input = response)
-				embed = discord.Embed(description = view.list[0], timestamp = datetime.datetime.now())
-				embed.set_footer(text = f"Page 1 of {len(view.list)}")
-				await ctx.send(embed = embed, view = view)
+		if self.bot.db['autoresponder'].count_documents({"guild": id}) == 0:
+			return await ctx.send("No autoresponse on this guild")
+
+		wild = ""
+		norm = ""
+		for i in self.bot.db['autoresponder'].find({'guild': id}):
+			if i['wildcard'] == True:
+				wild += f"• `\"{i['trigger']}\"` — `\"{i['response']}\"`"
 			else:
-				await ctx.send(embed = discord.Embed(description = response, color = 0x00FF77, timestamp = datetime.datetime.now()))
+				norm += f"• `\"{i['trigger']}\"` — `\"{i['response']}\"`"
+
+		outstr = "__**Normal autoresponses:**__\n\n {} \n\n __**Wildcards:**__\n\n {}".format(norm, wild)
+
+		if len(outstr) <= 4096:
+			await ctx.send(embed = discord.Embed(description = outstr, color = 0x00FF77, timestamp = datetime.datetime.now()))
+			return
+			
+		view = PaginatorView(input = outstr)
+		embed = discord.Embed(description = view.list[0], timestamp = datetime.datetime.now())
+		embed.set_footer(text = f"Page 1 of {len(view.list)}")
+		await ctx.send(embed = embed, view = view)
+		
+				
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		id = guildid(message.guild.id)
+		
+		check = self.bot.db['Guild settings'].find_one({'_id': id})['autoresponder']
+		
+		if (not check) or (message.guild == None) or message.author.bot:
+			return
+			
+		dcheck = self.bot.db['Guild settings'].find_one({'_id': id})['dadmode']
+
+		msg = re.sub('[.,;:?!/\-\'\"]', '', message.content).lower()
+
+		normal = [(i['trigger'], i['response']) for i in self.bot.db['autoresponder'].find({'guild': id, 'wildcard': False})]
+		wild = [(i['trigger'], i['response']) for i in self.bot.db['autoresponder'].find({'guild': id, 'wildcard': True})]
+
+		for i in normal:
+			if message.content.lower() == i[0].lower():
+				async with message.channel.typing():
+					await asyncio.sleep(0.5)
+					return await message.reply(i[1])
+
+		for i in wild:
+			if (msg.startswith(f"{i[0]} ")) or (msg.endswith(f" {i[0]}")) or (f" {i[0]} " in msg) or (msg == i[0]):
+				async with message.channel.typing():
+					await asyncio.sleep(0.5)
+					return await message.reply(i[1])
+
+		if any([" i'm " in msg, " im " in msg, " i am " in msg, msg.startswith('im '), msg.startswith("i'm "), msg.startswith('i am ')]) and dcheck and len(msg) <= 2035:
+			pos = None
+			for i in ["im", "i am", "i'm"]:
+				match = re.match(i, msg)
+				if match != None:
+					pos = match.end()
+					break
+			if pos != None:
+				reply = f"Hi{msg[pos:]}, I\'m dad!"
+				async with message.channel.typing():
+					await asyncio.sleep(0.5)
+					return await message.reply(reply)
 	
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
 		if member.guild.id == 612234021388156938:
 			channel = self.bot.get_channel(612251604132823071)
-			await asyncio.sleep(0.5)
 			await channel.send(f'''Welcome to HoQ {member.mention}! To get verified, post a link to your Quora account. If you do not have a Quora account, let us know how you found the server.
 Thank you for your patience! A moderator will verify you shortly.''')
 
